a8990ed31019d3d640ce694e9ad9a8ffe29b5701
Yauheni Kapachenia <kapachenia@gmail.com>
2021-09-16 16:36:31 +0300

    create a starting layout

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header className='header'>
+            <img src="https://i.pinimg.com/originals/7f/e1/92/7fe192806e4f48dad849363337ea0f66.png" alt=""/>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Profile.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function Profile() {
+    return (
+        <div className='content'>
+            <div>
+                <img
+                    src="https://www.rgo.ru/sites/default/files/styles/full_view/public/media/2020-12-14/peyzazh_stepanenko_nikolay_-_dolina_oseni_-_2020_-_515869.jpg?itok=BtvZDoAB"
+                    alt=""/>
+            </div>
+            <div>ava + description</div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
